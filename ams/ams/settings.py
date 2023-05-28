bc200f6ad0aea76d396c1ed96ef1669f088634a1
Nico Julian <NicoGiuliani@users.noreply.github.com>
2023-05-27 23:54:55 -0700

    Redeploying; looks good this time.

--- ams/ams/settings.py
+++ ams/ams/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if not DEBUG:
     CSRF_TRUSTED_ORIGINS = ['https://django-ams-production.up.railway.app']
