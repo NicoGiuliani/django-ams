6fae2c73b832b2cf871e57ca93db532fcdeaf06a
Nico Julian <NicoGiuliani@users.noreply.github.com>
2023-05-27 23:23:38 -0700

    Debug

--- ams/ams/settings.py
+++ ams/ams/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenois,e.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
